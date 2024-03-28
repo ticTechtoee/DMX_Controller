54f1d21cc887db349eddf11ea00d0e19abb18724
Farhan Basheer <farhanfb6@outlook.com>
2024-03-28 19:29:48 +0500

    updated project

--- GMX_Controller/Form1.Designer.cs
+++ GMX_Controller/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnOn = new System.Windows.Forms.Button();
+            this.BtnOff = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.LblDetectDevice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnOn
+            // 
+            this.BtnOn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnOn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOn.ForeColor = System.Drawing.Color.White;
+            this.BtnOn.Location = new System.Drawing.Point(120, 108);
+            this.BtnOn.Name = "BtnOn";
+            this.BtnOn.Size = new System.Drawing.Size(115, 77);
+            this.BtnOn.TabIndex = 0;
+            this.BtnOn.Text = "On";
+            this.BtnOn.UseVisualStyleBackColor = false;
+            // 
+            // BtnOff
+            // 
+            this.BtnOff.BackColor = System.Drawing.Color.Red;
+            this.BtnOff.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOff.ForeColor = System.Drawing.Color.White;
+            this.BtnOff.Location = new System.Drawing.Point(387, 108);
+            this.BtnOff.Name = "BtnOff";
+            this.BtnOff.Size = new System.Drawing.Size(115, 77);
+            this.BtnOff.TabIndex = 1;
+            this.BtnOff.Text = "Off";
+            this.BtnOff.UseVisualStyleBackColor = false;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.Gray;
+            this.BtnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExit.ForeColor = System.Drawing.Color.White;
+            this.BtnExit.Location = new System.Drawing.Point(496, 271);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(101, 43);
+            this.BtnExit.TabIndex = 2;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = false;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // LblDetectDevice
+            // 
+            this.LblDetectDevice.AutoSize = true;
+            this.LblDetectDevice.Location = new System.Drawing.Point(13, 301);
+            this.LblDetectDevice.Name = "LblDetectDevice";
+            this.LblDetectDevice.Size = new System.Drawing.Size(37, 13);
+            this.LblDetectDevice.TabIndex = 3;
+            this.LblDetectDevice.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(609, 326);
+            this.Controls.Add(this.LblDetectDevice);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnOff);
+            this.Controls.Add(this.BtnOn);
+            this.Name = "Form1";
+            this.Text = "Main Page";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnOn;
+        private System.Windows.Forms.Button BtnOff;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label LblDetectDevice;
     }
 }
 
--- GMX_Controller/Form1.cs
+++ GMX_Controller/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMX_Controller;
+
 
 namespace GMX_Controller
 {
@@ -15,6 +17,32 @@ namespace GMX_Controller
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //OpenDMX.start();
+            if (OpenDMX.IsDeviceConnected())
+            {
+                LblDetectDevice.Text = "DMX Device Detected";
+            }
+            else
+            {
+                LblDetectDevice.Text = "No DMX Device Detected";
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
         }
     }
 }
